aca4b58f00c79f325f100867e06515d00c5c0f7d
James McGrory <james.mcgrory135@gmail.com>
2023-02-22 14:33:10 +1100

    all products page desktop and mobile responsive

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -2,24 +2,21 @@ import React from "react";
 
 export default function ProductCard({img, bean, title, price}) {
     return (
-        <div className="bg-[#f3d7a8] text-stone-800 py-6 px-4 h-[28rem] w-64 shrink-0 rounded-md flex flex-col justify-between cursor-pointer">
-            <span className="bg-orange-900 text-stone-200 py-1 pl-2 rounded-2xl w-16 text-xs">
+        <div className="bg-[#f3d7a8] text-stone-800  py-2 px-2 h-[24rem] w-56 shrink-0 rounded-md flex flex-col cursor-pointer">
+            <span className="w-16 py-1 pl-2 text-xs bg-orange-900 text-stone-200 rounded-2xl">
                 {bean}
             </span>
-            <div className="flex justify-center">
+            <div className="flex justify-center mt-2">
                 <img src={img} alt="best seller coffee" className="h-56" />
             </div>
-            <p className="text-2xl font-bold">
+            <p className="mt-4 text-2xl">
                 {title}
             </p>
-            <div className="flex justify-between items-center">
-                <button className="bg-orange-500 px-5 py-2 text-stone-100 rounded-md hover:bg-orange-600">
-                    Order Now
-                </button>
-                <p className="font-bold">
-                    from ${price}.00
-                </p>
-            </div>
+            <p>
+                from ${price}.00
+            </p>
         </div>
     )
 }
+
+//bg-[#f3d7a8]
--- src/Pages/AllProducts.jsx
+++ src/Pages/AllProducts.jsx
@@ -4,17 +4,22 @@ import ProductCard from "../Components/ProductCard";
 
 export default function AllProducts() {
     return (
-        <section className="flex flex-wrap gap-4">
-            {products.map(product => {
-                return <ProductCard 
-                            key={product.id} 
-                            id={product.id}
-                            img={product.image}
-                            bean={product.bean}
-                            title={product.title}
-                            price={product.price}
-                        />
-            })}
+        <section className="flex flex-col pb-8 items-center gap-4 bg-[#f3d7a8] w-full">
+            <h2 className="my-8 text-4xl font-bold text-stone-800">
+                All Coffee
+            </h2>
+            <div className="flex flex-wrap justify-center max-w-6xl gap-4">
+                {products.map(product => {
+                    return <ProductCard 
+                                key={product.id} 
+                                id={product.id}
+                                img={product.image}
+                                bean={product.bean}
+                                title={product.title}
+                                price={product.price}
+                            />
+                })}
+            </div>
         </section>
     )
 }
