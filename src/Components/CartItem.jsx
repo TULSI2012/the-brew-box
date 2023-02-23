3b9099ea8f70158b4849d2a64e0433b4cccc15c5
James McGrory <james.mcgrory135@gmail.com>
2023-02-23 19:24:12 +1100

    checkout mobile view complete

--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -8,14 +8,24 @@ export default function CartItem(props) {
     console.log(props.id)
 
     return (
-        <div>
-            <img src={props.image} alt="cart-item" className="w-24" />
-            <h1>{props.title}</h1>
-            <p>{props.price}</p>
-            <div>
-                <button onClick={() => removeFromCart(props.id)}>-</button>
-                <p>{cartItems[props.id]}</p>
-                <button onClick={() => addToCart(props.id)}>+</button>
+        <div className="flex gap-x-4">
+            <div className="px-6 py-3 bg-gray-100 rounded-lg">
+                <img src={props.image} alt="cart-item" className="w-16" />
+            </div>
+            <div className="flex flex-col justify-between">
+                <div>
+                    <h1 className="font-semibold">{props.title}</h1>
+                    <p className="text-sm text-gray-600">${props.price}.00</p>
+                </div>
+                <div className="flex items-center w-24 pb-2 gap-x-4">
+                    <button
+                        className="items-center w-8 text-gray-600 border rounded-full" 
+                        onClick={() => removeFromCart(props.id)}>-</button>
+                    <p>{cartItems[props.id]}</p>
+                    <button 
+                        className="items-center w-8 text-gray-500 border rounded-full" 
+                        onClick={() => addToCart(props.id)}>+</button>
+                </div>
             </div>
         </div>
     )
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -5,22 +5,43 @@ import CartItem from "../Components/CartItem";
 
 export default function Cart() {
 
+    {/* Pulling in necessary state and function from the ShopContext*/}
     const { cartItems, getTotalCartAmount } = useContext(ShopContext)
     
-    const totalAmount = getTotalCartAmount()
+    {/* Implementing the getTotalCartAmount function from the ShopContext and saving the value to a variable*/}
+    const totalAmount = getTotalCartAmount() // ie totalAmount = 15
 
     return (
-        <section>
-            <h1>Your Cart</h1>
-            <div>
+        <section className="min-h-screen px-6 py-10">
+            <h1 className="text-4xl text-center">Your Cart</h1>
+            <div className="flex flex-col mt-10 gap-y-8">
                 {products.map((product) => {
                     if (cartItems[product.id] !== 0) {
                         return <CartItem key={product.id} id={product.id} title={product.title} price={product.price} image={product.image}/>
                     }
                 })}
             </div>
+            <div className="flex flex-col mt-10 mb-8 gap-y-4">
+                <p className="text-xl font-semibold">Order Summary</p>
+                <div>
+                <div className="flex justify-between">
+                    <p className="text-sm text-gray-500">Subtotal:</p>
+                    <p className="text-sm text-gray-500">${totalAmount}</p>
+                </div>
+                <div className="flex justify-between">
+                    <p className="text-sm text-gray-500">Shipping:</p>
+                    <p className="text-sm text-gray-500">+$10.00</p>
+                </div>
+                </div>
+                <div className="flex justify-between">
+                    <p className="text-sm text-gray-500">Total:</p>
+                    <p className="font-semibold">${totalAmount + 10}.00</p>
+                </div>
+            </div>
             <div>
-                <p>total: ${totalAmount}</p>
+                <button className="w-full py-2 font-semibold text-white bg-orange-400 rounded-lg">
+                    Checkout
+                </button>
             </div>
         </section>
     )
