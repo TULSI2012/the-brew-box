d91fd52695df6a11720345106a4f649b7a2b2fbd
James McGrory <james.mcgrory135@gmail.com>
2023-02-25 16:46:56 +1100

    Collection section mobile responsive

--- src/Components/Collections.jsx
+++ src/Components/Collections.jsx
@@ -2,52 +2,51 @@ import React from "react";
 
 export default function Collections() {
     return (
-        <section className="bg-[#f3d7a8] p-10 flex flex-col gap-y-4">
-            <div className="w-full h-48 relative group cursor-pointer">
-                <img src="./src/Images/coffee-cups.jpg" alt="coffee-cup" className="object-cover h-48 w-full brightness-75" />
-                <p className="text-stone-100 font-bold text-2xl px-4 py-2 drop-shadow-xl group-hover:-translate-y-2 transition ease-in-out absolute bottom-0 left-0">
-                    Roasters Choice Collection
-                </p>
-            </div>
-            <div className="flex gap-x-4">
-                <div className="w-full h-48 relative group cursor-pointer">
-                    <img src="./src/Images/coffee-cups.jpg" alt="coffee-cup" className="object-cover h-48 w-full brightness-75" />
-                    <p className="text-stone-100 font-bold text-2xl px-4 py-2 drop-shadow-xl group-hover:-translate-y-2 transition ease-in-out absolute bottom-0 left-0">
-                        Seasonal Collection
+        <section className="bg-[#f3d7a8] p-10 flex flex-col gap-y-4 lg:flex-row lg:justify-center lg:gap-x-4">
+            <div className="flex flex-col w-full lg:max-w-lg gap-y-4">
+                <div className="relative w-full h-48 cursor-pointer group">
+                    <img src="./src/Images/coffee-cups.jpg" alt="coffee-cup" className="object-cover w-full h-48 brightness-75" />
+                    <p className="absolute bottom-0 left-0 px-4 py-2 text-2xl font-bold transition ease-in-out text-stone-100 drop-shadow-xl group-hover:-translate-y-2">
+                        Roasters Choice Collection
                     </p>
                 </div>
-                <div className="w-full h-48 relative group cursor-pointer">
-                    <img src="./src/Images/coffee-cups.jpg" alt="coffee-cup" className="object-cover h-48 w-full brightness-75" />
-                    <p className="text-stone-100 font-bold text-2xl px-4 py-2 drop-shadow-xl group-hover:-translate-y-2 transition ease-in-out absolute bottom-0 left-0">
-                        Espresso Collection
-                    </p>
-                </div>
-            </div>
-            <div className="flex gap-x-4">
-                <div className="w-full h-48 relative group cursor-pointer">
-                    <img src="./src/Images/coffee-cups.jpg" alt="coffee-cup" className="object-cover h-48 w-full brightness-75" />
-                    <p className="text-stone-100 font-bold text-2xl px-4 py-2 drop-shadow-xl group-hover:-translate-y-2 transition ease-in-out absolute bottom-0 left-0">
-                        Ethiopia Collection
-                    </p>
-                </div>
-                <div className="w-full h-48 relative group cursor-pointer">
-                    <img src="./src/Images/coffee-cups.jpg" alt="coffee-cup" className="object-cover h-48 w-full brightness-75" />
-                    <p className="text-stone-100 font-bold text-2xl px-4 py-2 drop-shadow-xl group-hover:-translate-y-2 transition ease-in-out absolute bottom-0 left-0">
-                       Coloumbia Collection
-                    </p>
+                <div className="flex gap-x-4">
+                    <div className="relative w-full h-48 cursor-pointer group">
+                        <img src="./src/Images/coffee-cups.jpg" alt="coffee-cup" className="object-cover w-full h-48 brightness-75" />
+                        <p className="absolute bottom-0 left-0 px-4 py-2 text-2xl font-bold transition ease-in-out text-stone-100 drop-shadow-xl group-hover:-translate-y-2">
+                            Seasonal Collection
+                        </p>
+                    </div>
+                    <div className="relative w-full h-48 cursor-pointer group">
+                        <img src="./src/Images/coffee-cups.jpg" alt="coffee-cup" className="object-cover w-full h-48 brightness-75" />
+                        <p className="absolute bottom-0 left-0 px-4 py-2 text-2xl font-bold transition ease-in-out text-stone-100 drop-shadow-xl group-hover:-translate-y-2">
+                            Espresso Collection
+                        </p>
+                    </div>
                 </div>
             </div>
-            <div className="w-full h-48 relative group cursor-pointer">
-                <img src="./src/Images/coffee-cups.jpg" alt="coffee-cup" className="object-cover h-48 w-full brightness-75" />
-                <p className="text-stone-100 font-bold text-2xl px-4 py-2 drop-shadow-xl group-hover:-translate-y-2 transition ease-in-out absolute bottom-0 left-0">
-                    Signature Collection
-                </p>
+            <div className="flex flex-col w-full lg:max-w-lg gap-y-4">
+                <div className="flex gap-x-4">
+                    <div className="relative w-full h-48 cursor-pointer group">
+                        <img src="./src/Images/coffee-cups.jpg" alt="coffee-cup" className="object-cover w-full h-48 brightness-75" />
+                        <p className="absolute bottom-0 left-0 px-4 py-2 text-2xl font-bold transition ease-in-out text-stone-100 drop-shadow-xl group-hover:-translate-y-2">
+                            Ethiopia Collection
+                        </p>
+                    </div>
+                    <div className="relative w-full h-48 cursor-pointer group">
+                        <img src="./src/Images/coffee-cups.jpg" alt="coffee-cup" className="object-cover w-full h-48 brightness-75" />
+                        <p className="absolute bottom-0 left-0 px-4 py-2 text-2xl font-bold transition ease-in-out text-stone-100 drop-shadow-xl group-hover:-translate-y-2">
+                        Coloumbia Collection
+                        </p>
+                    </div>
+                </div>
+                <div className="relative w-full h-48 cursor-pointer group">
+                    <img src="./src/Images/coffee-cups.jpg" alt="coffee-cup" className="object-cover w-full h-48 brightness-75" />
+                    <p className="absolute bottom-0 left-0 px-4 py-2 text-2xl font-bold transition ease-in-out text-stone-100 drop-shadow-xl group-hover:-translate-y-2">
+                        Signature Collection
+                    </p>
+                </div>
             </div>
         </section>
     )
 }
-
-/* improvements:
-    -hover effects: underline text and add movement to image
-    -desktop responsiveness: flip grid so that its two rows high
-*/
