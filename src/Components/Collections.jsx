dfae7238584a3d7a5703238bc8601eb79759f81a
James McGrory <james.mcgrory135@gmail.com>
2023-02-27 18:14:21 +1100

    updated hero section - mobile + desktop responsive

--- src/Components/Collections.jsx
+++ src/Components/Collections.jsx
@@ -6,20 +6,20 @@ export default function Collections() {
             <div className="flex flex-col w-full lg:max-w-lg gap-y-4">
                 <div className="relative w-full h-48 cursor-pointer group">
                     <img src="./src/Images/coffee-cups.jpg" alt="coffee-cup" className="object-cover w-full h-48 brightness-75" />
-                    <p className="absolute bottom-0 left-0 px-4 py-2 text-2xl font-bold transition ease-in-out text-stone-100 drop-shadow-xl group-hover:-translate-y-2">
+                    <p className="absolute bottom-0 left-0 px-4 py-2 text-2xl font-bold transition duration-500 ease-in-out text-stone-100 drop-shadow-xl group-hover:-translate-y-2">
                         Roasters Choice Collection
                     </p>
                 </div>
                 <div className="flex gap-x-4">
                     <div className="relative w-full h-48 cursor-pointer group">
                         <img src="./src/Images/coffee-cups.jpg" alt="coffee-cup" className="object-cover w-full h-48 brightness-75" />
-                        <p className="absolute bottom-0 left-0 px-4 py-2 text-2xl font-bold transition ease-in-out text-stone-100 drop-shadow-xl group-hover:-translate-y-2">
+                        <p className="absolute bottom-0 left-0 px-4 py-2 text-2xl font-bold transition duration-500 ease-in-out text-stone-100 drop-shadow-xl group-hover:-translate-y-2">
                             Seasonal Collection
                         </p>
                     </div>
                     <div className="relative w-full h-48 cursor-pointer group">
                         <img src="./src/Images/coffee-cups.jpg" alt="coffee-cup" className="object-cover w-full h-48 brightness-75" />
-                        <p className="absolute bottom-0 left-0 px-4 py-2 text-2xl font-bold transition ease-in-out text-stone-100 drop-shadow-xl group-hover:-translate-y-2">
+                        <p className="absolute bottom-0 left-0 px-4 py-2 text-2xl font-bold transition duration-500 ease-in-out text-stone-100 drop-shadow-xl group-hover:-translate-y-2">
                             Espresso Collection
                         </p>
                     </div>
@@ -29,20 +29,20 @@ export default function Collections() {
                 <div className="flex gap-x-4">
                     <div className="relative w-full h-48 cursor-pointer group">
                         <img src="./src/Images/coffee-cups.jpg" alt="coffee-cup" className="object-cover w-full h-48 brightness-75" />
-                        <p className="absolute bottom-0 left-0 px-4 py-2 text-2xl font-bold transition ease-in-out text-stone-100 drop-shadow-xl group-hover:-translate-y-2">
+                        <p className="absolute bottom-0 left-0 px-4 py-2 text-2xl font-bold transition duration-500 ease-in-out text-stone-100 drop-shadow-xl group-hover:-translate-y-2">
                             Ethiopia Collection
                         </p>
                     </div>
                     <div className="relative w-full h-48 cursor-pointer group">
                         <img src="./src/Images/coffee-cups.jpg" alt="coffee-cup" className="object-cover w-full h-48 brightness-75" />
-                        <p className="absolute bottom-0 left-0 px-4 py-2 text-2xl font-bold transition ease-in-out text-stone-100 drop-shadow-xl group-hover:-translate-y-2">
+                        <p className="absolute bottom-0 left-0 px-4 py-2 text-2xl font-bold transition duration-500 ease-in-out text-stone-100 drop-shadow-xl group-hover:-translate-y-2">
                         Coloumbia Collection
                         </p>
                     </div>
                 </div>
                 <div className="relative w-full h-48 cursor-pointer group">
                     <img src="./src/Images/coffee-cups.jpg" alt="coffee-cup" className="object-cover w-full h-48 brightness-75" />
-                    <p className="absolute bottom-0 left-0 px-4 py-2 text-2xl font-bold transition ease-in-out text-stone-100 drop-shadow-xl group-hover:-translate-y-2">
+                    <p className="absolute bottom-0 left-0 px-4 py-2 text-2xl font-bold transition duration-500 ease-in-out text-stone-100 drop-shadow-xl group-hover:-translate-y-2">
                         Signature Collection
                     </p>
                 </div>
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,17 +3,14 @@ import { Link } from "react-router-dom";
 
 export default function Hero() {
     return (
-        <section className="bg-[#31452C] text-stone-300 h-full flex flex-col items-center py-10">
-            <h1 className="mx-5 text-5xl font-bold text-center max-w-xl text-stone-200">Sip into something exceptional</h1>
-            <p className="text-center mt-10 mx-16 text-l leading-6 max-w-lg">
+        <section className="bg-[url('./Images/hero-bg.jpg')] flex flex-col h-screen bg-cover bg-top py-10 lg:[background-position-x:250px] bg-no-repeat bg-black lg:py-28 lg:px-10">
+            <h1 className="max-w-2xl mx-5 text-6xl font-bold text-white lg:text-7xl">Sip into something exceptional</h1>
+            <p className="max-w-md mx-5 mt-4 leading-6 text-gray-100">
                 Elevate your coffee game and join the ranks of coffee
                 connoisseurs with our exceptional blends. Order now and start
                 your journey to coffee perfection.
             </p>
-            <div className="mt-28 mb-5 bg-[#f3d7a8] w-80 justify-center px-10">
-                <img src="./src/Images/coffee-bag.png" alt="american roast" className="-translate-y-16" />
-            </div>
-            <Link to="/all" className="bg-orange-500 mt-4 px-5 py-2 text-white font-bold rounded-md">
+            <Link to="/all" className="w-40 px-5 py-2 mx-5 mt-4 font-semibold text-orange-500 border-2 border-orange-500 rounded-md cursor-pointer bg-none hover:bg-orange-500 hover:text-black">
                 Explore Range
             </Link>
         </section>
